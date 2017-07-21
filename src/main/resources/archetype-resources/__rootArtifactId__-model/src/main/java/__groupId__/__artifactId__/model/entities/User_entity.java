408a6e0a57180e170705bb8c402fb43e2330b9c5
Mistifiou <mistifiou@yahoo.fr>
2017-07-21 11:56:58 +0200

    add User_entity entity

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-model/src/main/java/__groupId__/__artifactId__/model/entities/User_entity.java
@@ -0,0 +1,43 @@
+package ${groupId}.${artifactId}.model.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: User
+ *
+ */
+@Entity
+@Table(name="User_table")
+public class User_entity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private long id;
+	
+	@column(name="firstName")
+	private string firstName;
+
+	@column(name="subName")
+	private string subName;
+	
+	public string getSubName(){
+		return this.subName;
+	}
+	
+	public void setSubName(string name){
+		this.subName = subName;
+	}
+	
+	
+	public string getFirstName(){
+		return this.firstName;
+	}
+	
+	public void setFirstName(string name){
+		this.firstName = firstName;
+	}
+   
+}
